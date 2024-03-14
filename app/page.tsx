630134195f817c5455ed69a7d084f1dc5140eb45
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2024-03-14 16:39:02 +0700

    feat : add clerk auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Im a smart frontend developer</h1>
-      <h2 className="h1-bold">Im a smart frontend developer</h2>
-      <h3 className="h3-bold">Im a smart frontend developer</h3>
-    </div>
-  );
-}
