3e8f441d3198578122daff19f65c69d7a25cf051
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2024-03-15 15:04:19 +0700

    ui: add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,10 +41,7 @@ export default function RootLayout({
     >
       <html lang="en">
         <body className={`${inter.variable}${spaceGrotesk.variable}`}>
-          <ThemeProvider>
-            <h1 className="h1-bold">Bismillah Frontend Dev + benerin otak</h1>
-            {children}
-          </ThemeProvider>
+          <ThemeProvider>{children}</ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
