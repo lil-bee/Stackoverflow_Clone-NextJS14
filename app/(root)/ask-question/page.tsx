4ebd3d650d59500dfc57d84cb68fbd96e2f2210b
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2024-06-11 16:41:18 +0700

    ui: ask question form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,12 @@
+import { Question } from "@/components/shared/form/Question";
 import React from "react";
 
 const page = () => {
-  return <div>Ask Question</div>;
+  return (
+    <div>
+      <Question />
+    </div>
+  );
 };
 
 export default page;
