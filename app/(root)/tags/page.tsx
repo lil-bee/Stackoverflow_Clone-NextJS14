d34f79b53b8ab08c59fb818b828ced49a15797b0
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2024-06-21 00:26:39 +0700

    💄 UI : Tags Page

---
+++ app/(root)/tags/page.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
+import Filter from "@/components/shared/Filter";
+import { UserFilters } from "@/constants/filter";
+import UserCard from "@/components/shared/card/UserCard";
+import Link from "next/link";
+import { getAllTags } from "@/lib/actions/tag.action";
+import { Badge } from "@/components/ui/badge";
+
+const page = async () => {
+  const result = await getAllTags({});
+
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h2-bold text-dark100_light900">All Users</h1>
+      </div>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchBar
+          route="/"
+          placeholder="Search Questions"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          otherClasses="flex-1"
+        />
+        <Filter
+          containerClasses="flex"
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          filters={UserFilters}
+        />
+      </div>
+      <section className="mt-12 flex flex-wrap gap-4">
+        {result.tags.length > 0 ? (
+          result.tags.map((tag) => (
+            <Link
+              href={`/tags/${tag._id}`}
+              key={tag._id}
+              className="shadow-light100_darknone"
+            >
+              <article className="background-light900_dark200 light-border flex w-full flex-col rounded-2xl border px-8 py-10 sm:w-[260px]">
+                <div className="background-light800_dark400 w-fit rounded-sm px-5 py-1.5">
+                  <p className="paragraph-semibold text-dark300_light900">
+                    {tag.name}
+                  </p>
+                </div>
+
+                <p className="small-medium text-dark400_light500 mt-3.5">
+                  <span className="body-semibold primary-text-gradient mr-2.5">
+                    {tag.questions.length}+
+                  </span>{" "}
+                  Questions
+                </p>
+              </article>
+            </Link>
+          ))
+        ) : (
+          <Badge>No tags yet</Badge>
+        )}
+      </section>
+    </>
+  );
+};
+
+export default page;
