689a3f61f21924b27da80507f0ad6d5e95371d1c
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2024-06-24 22:01:17 +0700

    :lipstick: add question detail UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import "../styles/prism.css";
 import type { Metadata } from "next";
 import React from "react";
 import { ThemeProvider } from "@/context/ThemeProvider";
